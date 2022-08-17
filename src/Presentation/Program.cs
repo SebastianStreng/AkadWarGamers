415cca94e47a9407497f247914fb88f9c0cdf975
SebastianStreng <51957686+SebastianStreng@users.noreply.github.com>
2022-08-17 13:58:05 +0200

    finished

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Core;
+﻿using Core;
 using Application;
 
 namespace Presentation

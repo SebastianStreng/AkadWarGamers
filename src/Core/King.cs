24a44ca0f6edd2e756b21317ce614503baf38cb4
SebastianStreng <51957686+SebastianStreng@users.noreply.github.com>
2022-08-17 11:32:40 +0200

    add basic projet

---
+++ src/Core/King.cs
@@ -0,0 +1,24 @@
+﻿namespace Core;
+
+public class King
+{
+    public King(
+        string name,
+        int warriors,
+        double strength,
+        double weapon_strength,
+        double armor_strength)
+    {
+        this.name = name;
+        this.warriors = warriors;
+        this.strength = strength;
+        this.weapon_strength = weapon_strength;
+        this.armor_strength = armor_strength;
+    }
+
+    public string name { get; }
+    public int warriors { get; }
+    public double strength { get; }
+    private double weapon_strength { get; }
+    private double armor_strength { get; }
+}

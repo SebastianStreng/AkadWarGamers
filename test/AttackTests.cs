be6c7f5cb9f113f29f5198abba3d9f4273b88003
SebastianStreng <51957686+SebastianStreng@users.noreply.github.com>
2022-08-18 09:29:34 +0200

    finsihed tests

---
+++ test/AttackTests.cs
@@ -0,0 +1,44 @@
+﻿using Application;
+using Core;
+using NUnit.Framework;
+using System;
+
+namespace test
+{
+    [TestFixture]
+    public class AttackTests
+    {
+        private King TestKing1 = new King("King Arthur", 1000, 10, 15, 3);
+        private King TestKing2 = new King("King Ludwig", 1000, 10, 15, 3);
+
+        [Test]
+        public void attack_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+
+            // Act
+            Attack.attack(
+                TestKing1,
+                TestKing2);
+
+            // Assert
+            Assert.That(TestKing2.warriors == 700);
+        }
+
+        [Test]
+        public void runOver_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var numberOfWarriorsKing1 = TestKing1.warriors;
+            var numberOfWarriorsKing2 = TestKing2.warriors;
+
+            // Act
+            Attack.runOver(
+                TestKing1,
+                TestKing2);
+
+            // Assert
+            Assert.That(TestKing1.warriors > numberOfWarriorsKing1 && TestKing2.warriors < numberOfWarriorsKing2);
+        }
+    }
+}
--- test/SelectorTests.cs
+++ test/SelectorTests.cs
@@ -6,22 +6,23 @@ using System;
 namespace test
 {
     [TestFixture]
-    public class SpecialAttackTests
+    public class SelectorTests
     {
+        //To Test this Method, "decision" in Selector must be hardcoded.
         [Test]
-        public void startSpecialAttack_StateUnderTest_ExpectedBehavior()
+        public void SelectInteraction_StateUnderTest_ExpectedBehavior()
         {
             // Arrange
             King TestKing1 = new King("King Arthur", 1000, 10, 15, 3);
             King TestKing2 = new King("King Ludwig", 1000, 10, 15, 3);
 
             // Act
-            SpecialAttack.startSpecialAttack(
+            Selector.SelectInteraction(
                 TestKing1,
                 TestKing2);
 
             // Assert
-            Assert.That(SpecialAttack.checker == true);
+            Assert.That(Selector.checker == true);
         }
     }
 }

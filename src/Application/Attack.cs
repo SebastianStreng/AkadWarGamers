abcb0068eb3dd10fd2f4b69f68dadb1dd087e5c7
SebastianStreng <51957686+SebastianStreng@users.noreply.github.com>
2022-08-18 10:20:10 +0200

    add strenght-paramters

--- src/Application/Attack.cs
+++ src/Application/Attack.cs
@@ -11,7 +11,20 @@ namespace Application
     {
         public static void attack(King king1, King king2)
         {
+            var armeeStrenghtKing1 = king1.strength + king1.armor_strength + king1.weapon_strength;
+            var armeeStrenghtKing2 = king2.strength + king2.armor_strength + king2.weapon_strength;
+
             king2.warriors = Convert.ToInt32(king2.warriors * 0.7);
+
+            if (armeeStrenghtKing1 > armeeStrenghtKing2)
+            {
+                king2.warriors = Convert.ToInt32(king2.warriors - armeeStrenghtKing1);
+            }
+            if (armeeStrenghtKing1 < armeeStrenghtKing2)
+            {
+                king2.warriors = Convert.ToInt32(king2.warriors + armeeStrenghtKing2);
+            }
+
             Console.WriteLine($"{king1.name} has attacked {king2.name}´s armee. " +
                 $"\n {king2.name} has now {king2.warriors} warriors left.");
         }

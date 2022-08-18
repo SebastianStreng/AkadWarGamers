c3d20d854930b32b055acc6cada49928ded4b82f
SebastianStreng <51957686+SebastianStreng@users.noreply.github.com>
2022-08-18 10:39:05 +0200

    Update Attack.cs

--- src/Application/Attack.cs
+++ src/Application/Attack.cs
@@ -34,8 +34,12 @@ namespace Application
             int runOverIndicator = RandomGenerator.generateRandom(1, king2.warriors);
             king2.warriors = king2.warriors - runOverIndicator;
             king1.warriors = king1.warriors + runOverIndicator;
+            king1.weapon_strength = king1.weapon_strength * 1.2;
+            king1.armor_strength = king1.armor_strength * 1.2;
+            king1.strength = king1.strength * 1.2;
             Console.WriteLine($"{runOverIndicator} warriors left {king2.name}´s armee and switched sites. \n" +
-                $"{king1.name} has now {king1.warriors} warriors.");
+                $"{king1.name} has now {king1.warriors} warriors.\n" +
+                $"Each strength of {king1.name}´s armee increased by 20%");
         }
 
         public static void getSpecialAttack(King king1, King king2)
